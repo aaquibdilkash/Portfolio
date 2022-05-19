e6397ce66f9f34cd5b5f226add31fdaec722cf5f
Aaquib Dilkash <aaquibdilkash@gmail.com>
2022-05-19 16:43:22 +0530

    skills and experiences added

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href="https://www.linkedin.com/in/aaquibdilkash" target="_blank" rel="noreferrer">
+        <BsLinkedin />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
-    </div>
-    <div>
-      <BsInstagram />
+      <a href="https://github.com/aaquibdilkash" target="_blank" rel="noreferrer">
+        <BsGithub />
+      </a>
     </div>
   </div>
 );
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
       <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
 
       <div className="app__profiles">
-        {abouts.map((about, index) => (
+        {abouts?.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
